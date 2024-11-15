08d28c263a5544286d7737a2c81e1df1e089aff5
nikkfame <nikkfame@gmail.com>
2024-11-15 23:49:06 +0300

    Replace fmt with log for car dimensions output

--- main.go
+++ main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"auto/cars"
 	"auto/dimensions"
-	"fmt"
+	"log"
 )
 
 func main() {
- bmw := cars.BMW{}
- fmt.Println("BMW Dimensions:")
- fmt.Printf("Length: %.2f CM\n", bmw.Dimensions().Length().Get(dimensions.CM))
- fmt.Printf("Width: %.2f CM\n", bmw.Dimensions().Width().Get(dimensions.CM))
- fmt.Printf("Height: %.2f CM\n", bmw.Dimensions().Height().Get(dimensions.CM))
+bmw := cars.BMW{}
+	log.Println("BMW Dimensions:")
+	log.Printf("Length: %.2f CM\n", bmw.Dimensions().Length().Get(dimensions.CM))
+	log.Printf("Width: %.2f CM\n", bmw.Dimensions().Width().Get(dimensions.CM))
+	log.Printf("Height: %.2f CM\n", bmw.Dimensions().Height().Get(dimensions.CM))
 
- mercedes := cars.Mercedes{}
- fmt.Println("\nMercedes Dimensions:")
- fmt.Printf("Length: %.2f CM\n", mercedes.Dimensions().Length().Get(dimensions.CM))
- fmt.Printf("Width: %.2f CM\n", mercedes.Dimensions().Width().Get(dimensions.CM))
- fmt.Printf("Height: %.2f CM\n", mercedes.Dimensions().Height().Get(dimensions.CM))
+	mercedes := cars.Mercedes{}
+	log.Println("\nMercedes Dimensions:")
+	log.Printf("Length: %.2f CM\n", mercedes.Dimensions().Length().Get(dimensions.CM))
+	log.Printf("Width: %.2f CM\n", mercedes.Dimensions().Width().Get(dimensions.CM))
+	log.Printf("Height: %.2f CM\n", mercedes.Dimensions().Height().Get(dimensions.CM))
 
- dodge := cars.Dodge{}
- fmt.Println("\nDodge Dimensions:")
- fmt.Printf("Length: %.2f inches\n", dodge.Dimensions().Length().Get(dimensions.Inch))
- fmt.Printf("Width: %.2f inches\n", dodge.Dimensions().Width().Get(dimensions.Inch))
- fmt.Printf("Height: %.2f inches\n", dodge.Dimensions().Height().Get(dimensions.Inch))
+	dodge := cars.Dodge{}
+	log.Println("\nDodge Dimensions:")
+	log.Printf("Length: %.2f inches\n", dodge.Dimensions().Length().Get(dimensions.Inch))
+	log.Printf("Width: %.2f inches\n", dodge.Dimensions().Width().Get(dimensions.Inch))
+	log.Printf("Height: %.2f inches\n", dodge.Dimensions().Height().Get(dimensions.Inch))
 }
